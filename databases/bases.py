cc93b8350cd0e69411b6e8314176746ed83ff4c6
psihoshlem <yungync@gmail.com>
2023-02-12 22:04:43 +0300

    add db

--- databases/bases.py
+++ databases/bases.py
@@ -1,11 +1,50 @@
 import sqlite3
 
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('./databases/users.db')
 cur = conn.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS users(
-   userid INT PRIMARY KEY,
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
    login TEXT,
    password TEXT,
    salt TEXT);
 """)
 conn.commit()
+
+def add_new_user(user: dict):
+   user = (user["login"], user["password"], user["salt"])
+   cur.execute("INSERT INTO users (login, password, salt) VALUES(?, ?, ?);", user)
+   conn.commit()
+
+# для входа
+def get_user_data(login: str):
+   cur.execute(f"select * from users where login='{login}'")
+   print(cur.fetchall())
+
+
+def is_user_exist(login: str):
+   cur.execute(f"select * from users where login='{login}'")
+   return cur.fetchall()!=[]
+
+def delete_user_by_admin(login: str):
+   cur.execute(f"DELETE FROM users WHERE lname='{login}';")
+   conn.commit()
+
+
+def delete_user_by_admin(user: dict):
+   pass
+
+
+# get_user_data()
+# cur.execute(f"select * from users")
+# for item in cur.fetchall():
+#    print(item[1])
+
+users = [
+'senks',
+'root',
+'admin',
+'no_exist'
+]
+
+for user in users:
+   print(is_user_exist(user))
--- routers/log.py
+++ routers/log.py
@@ -4,6 +4,8 @@ import hashlib
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+from databases.bases import add_new_user, is_user_exist
+
 router = APIRouter()
 
 class User(BaseModel):
@@ -16,20 +18,18 @@ users = []
 async def reg(user: User):
     login = user.login
     password = user.password
-    for user in users:
-        if login==user['login']:
-            return False
-    
-    salt = os.urandom(32)
-    password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-    data={
-        'login':login,
-        'password':password,
-        'salt':salt
-    }
-    users.append(data)
-    print(users)
-    return True
+    if not is_user_exist(login):
+        salt = os.urandom(32)
+        password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+        data={
+            'login':login,
+            'password':password,
+            'salt':salt
+        }
+        add_new_user(data)
+        return True
+    else:
+        return False
 
 @router.post("/log")
 async def log(user: User):
